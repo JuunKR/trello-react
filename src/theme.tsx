0b961f18d3c66f365620bd9a9615092d559a351e
JuunKR <bluejun0510@naver.com>
2023-03-16 19:04:46 +0900

    first commit

---
+++ src/theme.tsx
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    textColor: "black",
+    bgColor: "#2f3640",
+    accentColor: "#4cd137"
+
+}
+
+
+export const lightTheme: DefaultTheme = {
+    textColor: "black",
+    bgColor: "whitesmoke",
+    accentColor: "#9c88ff"
+
+}
